424c5c6e9d4a1d8c18a5619d2fc655bf28aaa4a1
Shimrit Abraham <shimrit.sabraham@gmail.com>
2014-06-05 13:58:59 +0100

    First commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  DataManipulation
+//
+//  Created by Shimrit Abraham on 05/06/2014.
+//  Copyright (c) 2014 sabraham. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
