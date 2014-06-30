3b68361c1513bf228bba4e03fd55ec2c39d4b658
Shimrit Abraham <shimrit.sabraham@gmail.com>
2014-06-30 18:26:01 +0100

    Finally managed to write a generic templates cov() function. Struggled a lot with convering templated expressions and blocks to/from Matrix/Array objects. Found this solution online…

--- Eigen_MatrixBaseAddons.h
+++ Eigen_MatrixBaseAddons.h
@@ -9,22 +9,148 @@
 #ifndef DataManipulation_Eigen_MatrixBaseAddons_h
 #define DataManipulation_Eigen_MatrixBaseAddons_h
 
+#include <stdlib.h>
+#include <iostream>
+
 typedef Eigen::MatrixBase<Derived> EMatB;
-typedef shared_ptr<EMatB> pEMatB;
-typedef vector<pEMatB> v_pEMatB;
+typedef std::shared_ptr<EMatB> pEMatB;
+typedef std::vector<pEMatB> v_pEMatB;
 
 
-inline Scalar at(uint i, uint j) const { return this->operator()(i,j); }
+// ASK: I have not been able to separate the declaration from the definition of new methods here. Is this possible?
 
 
-Index myRows(){return this->rows();}
+Index myRows(){
+    return this->rows();
+}
 
-// Equally weighted past observations
-pEMatB equallyWeighted(const int lookback = -1) const; // negative lookback means 'use all data'
-v_pEMatB equallyWeightedRolling(const uint lookback) const;
 
-// EWMA-weighted past observations
-pEMatB ewmaWeighted(const double lambda) const;
-v_pEMatB ewmaWeightedRolling(const double lambda) const;
+// ------------------------------------------
+// Data Processing
+// ------------------------------------------
+
+
+
+
+
+template <typename OtherDerived>
+void
+diff(MatrixBase<OtherDerived> const & result_, const int lag = 0){
+    Index nRows  = this->rows();
+
+    // recommmended hack: get rid of constness in result
+    MatrixBase<OtherDerived>& result = const_cast< MatrixBase<OtherDerived>& >(result_);
+    result.derived().resize(nRows-abs(lag), this->cols());
+
+    if (lag<0)
+        result = this->bottomRows(nRows-abs(lag)) - this->topRows(nRows-abs(lag));
+    if (lag == 0)
+        throw std::string("ERROR:\t Lag parameter of zero is meaningless.")+
+        std::string("\nFILE:\t")+std::string(__FILE__)+std::string("\nROW:\t")+std::to_string(__LINE__);
+    if (lag>0)
+        result = this->topRows(nRows-abs(lag)) - this->bottomRows(nRows-abs(lag));
+}
+
+
+
+template <typename OtherDerived>
+void
+lnRet(MatrixBase<OtherDerived> const & result_, const int lag = -1) {
+    // lag<0: mat_t - mat_{t-|lag|}
+    // lag>0: mat_t - mat_{t+|lag|}
+    // lag = 0 is not allowed
+
+    Index nRows  = this->rows();
+    MatrixBase<OtherDerived>& result = const_cast< MatrixBase<OtherDerived>& >(result_);
+    result.derived().resize(nRows-abs(lag), this->cols());
+
+    if (lag<0)
+        result = this->bottomRows(nRows-abs(lag)).cwiseQuotient(this->topRows(nRows-abs(lag))) ;
+    if (lag == 0)
+        throw std::string("ERROR:\t Lag parameter of zero is meaningless.")+
+        std::string("\nFILE:\t")+std::string(__FILE__)+std::string("\nROW:\t")+std::to_string(__LINE__);
+    if(lag>0)
+        result = this->topRows(nRows-abs(lag)).cwiseQuotient(this->bottomRows(nRows-abs(lag))) ;
+
+
+    result = result.array().log();
+
+
+}
+
+
+
+// ------------------------------------------
+// Covariance Matrix: Equally weighted past observations
+// ------------------------------------------
+
+
+template <typename OtherDerived>
+void cov(const MatrixBase<Derived>& x, const MatrixBase<Derived>& y, MatrixBase<OtherDerived> const & C)
+{
+    typedef typename Derived::Scalar Scalar;
+    typedef typename internal::plain_row_type<Derived>::type RowVectorType;
+    const Scalar num_observations = static_cast<Scalar>(x.rows());
+
+    const RowVectorType x_mean = x.colwise().sum() / num_observations;
+    const RowVectorType y_mean = y.colwise().sum() / num_observations;
+
+    const_cast< MatrixBase<OtherDerived>& >(C) =
+        (x.rowwise() - x_mean).transpose() * (y.rowwise() - y_mean) / (num_observations-1);
+}
+
+//
+//template <typename OtherDerived>
+//void
+//Cov_equallyWeightedAllData(MatrixBase<OtherDerived> const & result_)const{
+//    // recommmended hack: get rid of constness in result
+//    MatrixBase<OtherDerived>& result = const_cast< MatrixBase<OtherDerived>& >(result_);
+//    result.derived().resize(this->cols(), this->cols());
+//
+//    const MatrixBase<Derived>> x_mean = this->colwise().sum();
+////    return pEMatB(new EMatB ((centered.transpose() * centered)/double(this->rows()))  );
+//}
+
+//
+//pEMatB
+//Cov_equallyWeighted(const int lookback = -1) {
+//    // lookback<0 means 'use all data'
+//
+//    Index N = this->rows();
+//
+//    if (lookback<0){
+//        // use all data to compute covariance matrix
+//        return this->Cov_EquallyWeightedAllData();
+//
+//    }else{
+//
+//        // compute rolling covariance matrix
+//        if(N<lookback+2)
+//            throw std::string("ERROR:\t Lookback parameter too large compared to nr of rows in Matrix")+
+//            std::string("\nFILE:\t")+std::string(__FILE__)+std::string("\nROW:\t")+std::to_string(__LINE__);
+//        return this->bottomRows(lookback)->Cov_EquallyWeighted();
+//    }
+//
+//}
+
+
+
+v_pEMatB Cov_equallyWeightedRolling(const uint lookback) const;
+
+
+
+
+
+
+// ------------------------------------------
+// Covariance Matrix: EWMA-weighted past observations
+// ------------------------------------------
+pEMatB Cov_ewmaWeighted(const double lambda) const;
+v_pEMatB Cov_ewmaWeightedRolling(const double lambda) const;
+
+
+
+
+
 
 #endif
--- main.cpp
+++ main.cpp
@@ -6,71 +6,110 @@
 //  Copyright (c) 2014 sabraham. All rights reserved.
 //
 
-#define EIGEN_MATRIXBASE_PLUGIN "Eigen_MatrixBaseAddons.h" // This HAS to be done first
+//#define EIGEN_MATRIXBASE_PLUGIN "Eigen_MatrixBaseAddons.h" // This HAS to be done first
+//
+//#include <iostream>
+//#include "boost/date_time/local_time/local_time.hpp"
+//#include "boost/date_time/posix_time/posix_time.hpp"
+//#include "boost/date_time/gregorian/gregorian.hpp"
+//#include "boost/shared_ptr.hpp"
+//#include "TimeSeriesFactory.h"
+//#include "LMatrix.h"
+//#include "TimeSeries.h"
+//#include "DateTimeLabels.h"
+//#include "StringLabels.h"
+//#include <string>
+//#include <vector>
+//#include "Utils.h"
+//#include <Eigen/Dense>
+//
+//
+//using namespace boost::posix_time;
+//using namespace boost::gregorian;
+//using namespace std;
+//using namespace Eigen;
+//// Tests
+////#include <igloo/igloo.h>
+////#include "Tests_Matrix.h"
+//
+//
+//typedef TimeSeries<double, DateTimeLabels, ptime> TSd;
+//typedef boost::shared_ptr<TimeSeries<double, DateTimeLabels, ptime>> pTSd;
+//typedef boost::shared_ptr<LMatrix<double, DateTimeLabels, ptime>> pLM;
+//
 
-#include <iostream>
-#include "boost/date_time/local_time/local_time.hpp"
-#include "boost/date_time/posix_time/posix_time.hpp"
-#include "boost/date_time/gregorian/gregorian.hpp"
-#include "boost/shared_ptr.hpp"
-#include "TimeSeriesFactory.h"
-#include "LMatrix.h"
-#include "TimeSeries.h"
-#include "DateTimeLabels.h"
-#include "StringLabels.h"
-#include <string>
-#include <vector>
-#include "Utils.h"
-#include <Eigen/Dense>
 
+//int main(int argc, const char * argv[])
+//{
+//    // settings
+//    string path = "/Users/shimritabraham/Documents/work/";
+//    string extension = ".csv";
+//    vector<string> symbols = {"GSPC", "GDAXI", "FTSE", "IXIC", "N225"};
+//    string dateTimeFormat = "%Y-%m-%d";
+//    vector<pTSd> to_join;
+//    string colNameExtension = ".Close";
+//
+//    // Read and process data for each symbol
+//    for (string s:symbols){
+//        string fullPath = path+s+extension;
+//        pTSd ts = TimeSeriesFactory::CreateTimeSeriesFromCsv<double, DateTimeLabels, ptime>(fullPath, SIMPLE, dateTimeFormat, true);
+//        pTSd colToJoin(new TSd (ts->col(s+colNameExtension)) );
+//        to_join.push_back(colToJoin);
+//    }
+//
+//    // Join the columns
+//    pTSd joined_cols = join(to_join);
+//    cout<<(*joined_cols)<<endl;
+//
+//    // Turn TimeSeries into Eigen::Matrix object
+//    Eigen::MatrixXd eigenMat = joined_cols->asEigenMatrix();
+//    cout<<eigenMat<<endl;
+//    cout<<eigenMat(1, 2)<<endl;
+//    cout<<eigenMat.myRows()<<endl;
+//    cout<<eigenMat.bottomRows(3).myRows()<<endl;
 
-using namespace boost::posix_time;
-using namespace boost::gregorian;
-using namespace std;
-using namespace Eigen;
-// Tests
-//#include <igloo/igloo.h>
-//#include "Tests_Matrix.h"
-
-
-typedef TimeSeries<double, DateTimeLabels, ptime> TSd;
-typedef boost::shared_ptr<TimeSeries<double, DateTimeLabels, ptime>> pTSd;
-typedef boost::shared_ptr<LMatrix<double, DateTimeLabels, ptime>> pLM;
-
-
-
-int main(int argc, const char * argv[])
-{
-    // settings
-    string path = "/Users/shimritabraham/Documents/work/";
-    string extension = ".csv";
-    vector<string> symbols = {"GSPC", "GDAXI", "FTSE", "IXIC", "N225"};
-    string dateTimeFormat = "%Y-%m-%d";
-    vector<pTSd> to_join;
-    string colNameExtension = ".Close";
-
-    // Read and process data for each symbol
-    for (string s:symbols){
-        string fullPath = path+s+extension;
-        pTSd ts = TimeSeriesFactory::CreateTimeSeriesFromCsv<double, DateTimeLabels, ptime>(fullPath, SIMPLE, dateTimeFormat, true);
-        pTSd colToJoin(new TSd (ts->col(s+colNameExtension)) );
-        to_join.push_back(colToJoin);
-    }
-
-    // Join the columns
-    pTSd joined_cols = join(to_join);
-    cout<<(*joined_cols)<<endl;
-
-    // Turn TimeSeries into Eigen::Matrix object
-    Eigen::MatrixXd eigenMat = joined_cols->asEigenMatrix();
-    cout<<eigenMat<<endl;
-    cout<<eigenMat(1, 2)<<endl;
-    cout<<eigenMat.myRows()<<endl;
 
 
     
 
-    return 0;
-    //return igloo::TestRunner::RunAllTests();
+//    return 0;
+//    //return igloo::TestRunner::RunAllTests();
+//}
+
+
+#include <iostream>
+#include <vector>
+#define EIGEN_MATRIXBASE_PLUGIN "Eigen_MatrixBaseAddons.h"
+#include <Eigen/Dense>
+using namespace std;
+
+
+int main()
+{
+    Eigen::Matrix<double, 7, 3, Eigen::RowMajor> a;
+
+    vector<double> r1{1, 2, 3};
+    vector<double> r2{1, 5, 3};
+    vector<double> r3{3, 3, 3};
+    vector<double> r4{15, 2, 3};
+    vector<double> r5{78, 12, 88};
+    vector<double> r6{90, 5, 9};
+    vector<double> r7{1, 22, 33};
+
+    vector<vector<double>> allVec{r1, r2, r3, r4, r5, r6, r7};
+
+    for (int i = 0; i < 7; i++)
+        a.row(i) = Eigen::Matrix<double, 3, 1>::Map(&allVec[i][0],allVec[i].size());
+
+    Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> res;
+    cout<<a<<endl;
+    cout<<endl;
+    a.lnRet(res, 1);
+    cout<<res<<endl;
+//    a.Cov_equallyWeightedAllData(res);
+    a.cov(a, a, res);
+    cout<<res<<endl;
+
+    
 }
 
